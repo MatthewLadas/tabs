aa5b85a4032d74034ab02728ec264f15d029f3af
MatthewLadas <ladas.matthew@gmail.com>
2022-04-20 18:28:26 -0400

    Completed Tabs project

--- tabs.js
+++ tabs.js
@@ -7,10 +7,19 @@ function handleTabClick(e) {
 	tabPanels.forEach(panel => {
 		panel.hidden = true;
 	});
+
 	// Mark all tabs as unselected
+	tabButtons.forEach(tab => {
+		tab.setAttribute('aria-selected', false); // tab.ariaSelected = false; DOESN'T WORK
+	});
 
 	// Mark the clicked tab as selected
+	e.currentTarget.setAttribute('aria-selected', true);
+
 	// Find the associated tabPanel and show it
+	const id = e.currentTarget.id;
+	const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+	tabPanel.hidden = false;
 }
 
 tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
