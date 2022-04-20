e34ab02c21e184e8a1f321abcb5b57308e2df839
MatthewLadas <ladas.matthew@gmail.com>
2022-04-19 20:04:00 -0400

    Created variables and hid all tabs

--- tabs.js
+++ tabs.js
@@ -1,1 +1,16 @@
-let age = 'five';
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(e) {
+	// Hide all tab panels
+	tabPanels.forEach(panel => {
+		panel.hidden = true;
+	});
+	// Mark all tabs as unselected
+
+	// Mark the clicked tab as selected
+	// Find the associated tabPanel and show it
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
